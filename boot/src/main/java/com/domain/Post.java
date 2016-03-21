f10ab0465a726338b4c1cc794e292854b2f68877
jaehokim <kjhihing@gmail.com>
2016-03-21 18:01:23 +0900

    spring boot sample

---
+++ boot/src/main/java/com/domain/Post.java
@@ -0,0 +1,54 @@
+package com.domain;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Post {
+	@Id
+	private int id;
+
+	private String subject;
+
+	private String content;
+
+	private Date regDate;
+
+	@GeneratedValue
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getSubject() {
+		return subject;
+	}
+
+	public void setSubject(String subject) {
+		this.subject = subject;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+	public Date getRegDate() {
+		return regDate;
+	}
+
+	public void setRegDate(Date regDate) {
+		this.regDate = regDate;
+	}
+
+}
